2b8a13a35d197dc00cd410bd3eafe51eb6f9e7dd
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2020-02-07 01:50:42 -0500

    Se ajustan cambios

--- src/app/pages/home-results/home-results.page.ts
+++ src/app/pages/home-results/home-results.page.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 import {
   NavController,
-  AlertController,
   MenuController,
-  ToastController,
-  PopoverController,
-  ModalController } from '@ionic/angular';
+  PopoverController
+} from '@ionic/angular';
 
-// Modals
-import { SearchFilterPage } from '../../pages/modal/search-filter/search-filter.page';
-import { ImagePage } from './../modal/image/image.page';
-// Call notifications test by Popover and Custom Component.
 import { NotificationsComponent } from './../../components/notifications/notifications.component';
 
 @Component({
@@ -19,20 +13,13 @@ import { NotificationsComponent } from './../../components/notifications/notific
   styleUrls: ['./home-results.page.scss']
 })
 export class HomeResultsPage {
-  searchKey = '';
-  yourLocation = '123 Test Street';
   themeCover = 'assets/img/clubes.jpg';
 
   constructor(
     public navCtrl: NavController,
     public menuCtrl: MenuController,
-    public popoverCtrl: PopoverController,
-    public alertCtrl: AlertController,
-    public modalCtrl: ModalController,
-    public toastCtrl: ToastController
-  ) {
-
-  }
+    public popoverCtrl: PopoverController
+  ) { }
 
   ionViewWillEnter() {
     this.menuCtrl.enable(true);
@@ -42,59 +29,6 @@ export class HomeResultsPage {
     this.navCtrl.navigateForward('settings');
   }
 
-  async alertLocation() {
-    const changeLocation = await this.alertCtrl.create({
-      header: 'Change Location',
-      message: 'Type your Address.',
-      inputs: [
-        {
-          name: 'location',
-          placeholder: 'Enter your new Location',
-          type: 'text'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Change',
-          handler: async (data) => {
-            console.log('Change clicked', data);
-            this.yourLocation = data.location;
-            const toast = await this.toastCtrl.create({
-              message: 'Location was change successfully',
-              duration: 3000,
-              position: 'top',
-              closeButtonText: 'OK',
-              showCloseButton: true
-            });
-
-            toast.present();
-          }
-        }
-      ]
-    });
-    changeLocation.present();
-  }
-
-  async searchFilter () {
-    const modal = await this.modalCtrl.create({
-      component: SearchFilterPage
-    });
-    return await modal.present();
-  }
-
-  async presentImage(image: any) {
-    const modal = await this.modalCtrl.create({
-      component: ImagePage,
-      componentProps: { value: image }
-    });
-    return await modal.present();
-  }
 
   async notifications(ev: any) {
     const popover = await this.popoverCtrl.create({
