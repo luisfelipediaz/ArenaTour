361e6d7fad816c5dc3eadd9dadf05ab7e178c4fb
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2020-02-28 11:58:42 -0500

    Se ajustan los usuarios

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,57 +5,66 @@ import * as corsConfig from 'cors';
 admin.initializeApp();
 const cors = corsConfig({ origin: true });
 
-export const userClaims = functions.auth.user().onCreate(async user => {
-    await admin.auth().setCustomUserClaims('1IIAMuFjNjWKIEmtx1XTTm4YVrg2', { admin: true, role: 'admin' });
-    await admin.auth().setCustomUserClaims('xJc2wMdBANXUqYcZKQfdCIr57Ul1', { admin: true, role: 'admin' });
-});
+export const setUserClaim = functions.https.onRequest((req, res) =>
+    cors(req, res, async () => {
+        const fnAdmin = async () => {
+            console.log(req.body);
+            const claims = { role: req.body.data.role, admin: false };
+            claims.admin = req.body.data.role === 'admin';
+            await admin.auth().setCustomUserClaims(req.body.data.uid, claims);
+            res.send({ data: 'Success' });
+        }
+        await adminFunction(req, res, fnAdmin);
+        return;
+    }));
 
 export const getUsers = functions.https.onRequest((req, res) =>
     cors(req, res, async () => {
-        if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
-            !(req.cookies && req.cookies.__session)) {
-            console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
-                'Make sure you authorize your request by providing the following HTTP header:',
-                'Authorization: Bearer <Firebase ID Token>',
-                'or by passing a "__session" cookie.');
+        const fnAdmin = async () => res.send({ data: await admin.auth().listUsers() });
+        await adminFunction(req, res, fnAdmin);
+        return;
+    }));
+
+async function adminFunction(req: functions.https.Request, res: functions.Response, fnAdmin: () => Promise<any>) {
+    if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
+        !(req.cookies && req.cookies.__session)) {
+        console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+            'Make sure you authorize your request by providing the following HTTP header:',
+            'Authorization: Bearer <Firebase ID Token>',
+            'or by passing a "__session" cookie.');
+        res.status(403).send('Unauthorized');
+        return;
+    }
+
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        console.log('Found "Authorization" header');
+        // Read the ID Token from the Authorization header.
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else if (req.cookies) {
+        console.log('Found "__session" cookie');
+        // Read the ID Token from cookie.
+        idToken = req.cookies.__session;
+    } else {
+        // No cookie
+        res.status(403).send('Unauthorized');
+        return;
+    }
+
+    try {
+        const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+        console.log('ID Token correctly decoded', decodedIdToken);
+        if (!decodedIdToken['admin']) {
+            console.log('User is not admin');
             res.status(403).send('Unauthorized');
             return;
-        }
-
-        let idToken;
-        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-            console.log('Found "Authorization" header');
-            // Read the ID Token from the Authorization header.
-            idToken = req.headers.authorization.split('Bearer ')[1];
-        } else if (req.cookies) {
-            console.log('Found "__session" cookie');
-            // Read the ID Token from cookie.
-            idToken = req.cookies.__session;
         } else {
-            // No cookie
-            res.status(403).send('Unauthorized');
-            return;
+            await fnAdmin();
         }
-
-        try {
-            const decodedIdToken = await admin.auth().verifyIdToken(idToken);
-            console.log('ID Token correctly decoded', decodedIdToken);
-            if (!decodedIdToken['admin']) {
-                console.log('User is not admin');
-                res.status(403).send('Unauthorized');
-                return;
-            } else {
-                res.send(
-                    {
-                        data: await admin.auth().listUsers()
-                    }
-                );
-            }
-            return;
-        } catch (error) {
-            console.error('Error while verifying Firebase ID token:', error);
-            res.status(403).send('Unauthorized');
-            return;
-        }
-    })
-);
+        return;
+    } catch (error) {
+        console.error('Error while verifying Firebase ID token:', error);
+        res.status(403).send('Unauthorized');
+        return;
+    }
+}
--- src/app/pages/profile-users/profile-users.page.ts
+++ src/app/pages/profile-users/profile-users.page.ts
@@ -11,16 +11,24 @@ import { map } from 'rxjs/operators';
 // tslint:disable-next-line: component-class-suffix
 export class ProfileUsersPage implements OnInit {
   getUsers$: (data: void) => Observable<{ users: (firebase.UserInfo & { customClaims: any })[] }>;
+  setUserClaim$: (data: { uid: string; role: string; }) => Observable<void>;
+
   users$: Observable<firebase.UserInfo[]>;
 
   constructor(
     private aff: AngularFireFunctions
   ) {
     this.getUsers$ = this.aff.httpsCallable<void, { users: (firebase.UserInfo & { customClaims: any })[] }>('getUsers');
+    this.setUserClaim$ = this.aff.httpsCallable<{ uid: string, role: string }, void>('setUserClaim');
   }
 
   ngOnInit() {
     this.users$ = this.getUsers$().pipe(map(data => data.users));
+  }
+
+  async changeRoleUser(newRole: { detail: { value: string } }, user: firebase.UserInfo) {
+    await this.setUserClaim$({ uid: user.uid, role: newRole.detail.value }).toPromise();
+    alert('Usuario actualizado con éxito');
   }
 
 }
